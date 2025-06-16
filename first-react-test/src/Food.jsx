91133029c611229874f14b7bdf7757076749cc5d
Luke <LukeSupan@outlook.com>
2025-06-16 18:12:08 -0400

    Setting up React

---
+++ first-react-test/src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+
+    const food1 = "Orange";
+    const food2 = "Apple";
+    const food3 = "Banana";
+
+    return (
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food3.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food;
---
+++ first-react-test/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>    
+            <p> Based on Bro Code React Full Course (2025), current year: ({new Date().getFullYear()})</p>
+        </footer>
+    );
+}
+
+export default Footer;
